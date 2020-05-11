d79e27a6ea9c7810b3bc740783ac31e2714fbc7a
Kent McKinney <kent.p.mckinney@gmail.com>
2020-05-11 09:34:51 -0700

    Updates

---
+++ precache-manifest.1ae427212b16ec1633f1e684b35cb428.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26e2b80205ff93369249bdd354fc68b4",
+    "url": "/epi-farmers-market/index.html"
+  },
+  {
+    "revision": "17b4a27494d039b960e3",
+    "url": "/epi-farmers-market/static/css/main.c272c1c5.chunk.css"
+  },
+  {
+    "revision": "1e2d5ad25d801137ac56",
+    "url": "/epi-farmers-market/static/js/2.014034bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/epi-farmers-market/static/js/2.014034bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17b4a27494d039b960e3",
+    "url": "/epi-farmers-market/static/js/main.63bdb112.chunk.js"
+  },
+  {
+    "revision": "266812405e4f75da90db",
+    "url": "/epi-farmers-market/static/js/runtime-main.aef4311f.js"
+  },
+  {
+    "revision": "2034149b3fff101e759479f7bb56cac9",
+    "url": "/epi-farmers-market/static/media/leaf.2034149b.png"
+  }
+]);
